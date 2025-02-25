e722d2c0d81bfd76a60d0cb280d24de7c8766bac
Aman-Sharma-03 <aman.sharma2022b@gmail.com>
2025-02-25 12:23:12 +0530

    pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
+const User = require("../models/user");
 
 const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "about","age", "gender", "skills"];
 
@@ -54,9 +55,17 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
                 { toUserId: loggedInUser._id, status: "accepted"}
             ],
             
-        }).populate("fromUserId", USER_SAFE_DATA);
+        }).populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA);
 
-        const data = sentConnectionRequests.map((row) => row.fromUserId);
+        const data = sentConnectionRequests.map((row) => {
+            // cannot compare to mongoose object id => coverting to the string and then checking
+            // accepted status can be from the fromUser or toUser
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
 
         res.json({
             message: "Data fetched Successfully",
@@ -64,6 +73,49 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         })
     } catch( err){
         res.status(400).send("ERROR: "+err.message);
+    }
+})
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+    try{
+        // User should see all the card except 
+        // 0. his own
+        // 1. his connections
+        // 2. ignored people
+        // 3. already sent the connectio request
+
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        const skip = (page-1)*limit;
+        limit = limit > 50 ? 50 : limit;
+
+        // Don't want these users in our feed
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+
+        // Including our own profile
+        const hideUserFromFeed = new Set();
+        connectionRequests.forEach((req) => {
+            hideUserFromFeed.add(req.fromUserId.toString());
+            hideUserFromFeed.add(req.toUserId.toString());
+        })
+
+        const users = await User.find({
+            $and: [
+                {_id: { $nin: Array.from(hideUserFromFeed) }},
+                {_id: { $ne: loggedInUser._id}}
+            ]
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+        res.json({data: users});
+    } catch(err) {
+        res.status(400).json({message: err.message});
     }
 })
 
