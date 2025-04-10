0f47ff20ad958a2dd9b98e2f91d1569c8a04f174
Aman-Sharma-03 <aman.sharma2022b@gmail.com>
2025-04-10 21:03:46 +0530

    fix: origin fix

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 // const bodyParser = require("./custom/bodyParser");
-const bodyParser = require('body-parser');
-const { connectDB } = require('./config/database');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const { connectDB } = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Custom BodyParser
 
 const allowedOrigins = [
-  'http://localhost:5173',
-  'https://dev-tinder-web-eta.vercel.app',
-  'https://devhive.jaii.in/login',
+  "http://localhost:5173",
+  "https://dev-tinder-web-eta.vercel.app",
+  "https://devhive.jaii.in",
 ];
 
 app.use(
@@ -22,7 +22,7 @@ app.use(
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, origin);
       } else {
-        callback(new Error('Not allowed by CORS'));
+        callback(new Error("Not allowed by CORS"));
       }
     },
     credentials: true,
@@ -31,25 +31,25 @@ app.use(
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const authRouter = require('./routes/auth');
-const profileRouter = require('./routes/profile');
-const requestRouter = require('./routes/request');
-const userRouter = require('./routes/user');
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
 
-app.use('/', authRouter);
-app.use('/', profileRouter);
-app.use('/', requestRouter);
-app.use('/', userRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log('Server running at port: ', PORT, '...');
+      console.log("Server running at port: ", PORT, "...");
     });
-    console.log('Database connection established...');
+    console.log("Database connection established...");
   })
   .catch((err) => {
-    console.log('Database cannot be connected!!' + err);
+    console.log("Database cannot be connected!!" + err);
   });
